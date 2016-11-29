3acf2790a2fffce38d79a9089be23a17ec435520
Krishna Kandula <krishna.c.kandula@gmail.com>
2016-11-29 15:08:30 -0600

    Added error handling for json response

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ request({
 	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddr}`,
 	json: true
 }, (error, response, body) => {
-	console.log(`Address: ${body.results[0].formatted_address}`);
-	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	if(error)
+		console.log('ERROR: Could not access Google servers');
+	else if(body.status === 'ZERO_RESULTS')
+		console.log(`Could not find address: ${argv.address}`);
+	else if(body.status === 'OK'){
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	}
 });
